048d68be7b9195069bd73d6b1318b68d8d2a0bd5
Andrey Nikitin <andrew@a71.su>
2026-02-13 13:02:57 +0300

    Move regex outside and add an example

--- source/templateEngine.js
+++ source/templateEngine.js
@@ -6,15 +6,27 @@
 // while trying to figure out how to remove ugly `if` checks. Pretty neat!
 
 /**
+ * Regular expression that matches template variables.
+ *
+ * @type {RegExp}
+*/
+const TEMPLATE_REGEX = /{{\s*([^{}\s]*)\s*}}/g;
+
+/**
  * Parses a template string.
  *
  * @param {string} template - The template to parse.
  * @param {object} data - The object holding values referenced in the template.
  * @returns {string} The parsed string.
+ *
+ * @example
+ * const template = "Hello, {{name}}!";
+ * const data = { name: "World" };
+ * templateEngine(template, data);
+ * // returns "Hello, World!"
  */
 function templateEngine(template, data) {
-    const re = /{{\s*([^{}\s]*)\s*}}/g;
-    return template.replaceAll(re, (_, capture) => {
+    return template.replaceAll(TEMPLATE_REGEX, (_, capture) => {
         return capture.split(".").reduce((obj, key) => obj?.[key], data) ?? "";
     });
 }
